66b8847bf8cdfdbad1bb4605434cfd60dcf23b1a
D Keith Casey Jr <keith@caseysoftware.com>
2013-02-22 12:09:02 -0600

    simple call forwarding

---
+++ rev1.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'creds.php';
+include 'vendor/autoload.php';
+
+header("Content-type: text/xml");
+
+/**
+ * Simple call forwarding
+ *
+ * The library below generates this:
+ * <Response>
+ *   <Dial>703-555-1212</Dial>
+ * </Response>
+ */
+
+$response = new Services_Twilio_Twiml();
+$response->dial($toPhone);
+
+print $response;
