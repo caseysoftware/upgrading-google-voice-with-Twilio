fc000312a88457e0aa62d907a76a4c14dd4528cb
D Keith Casey Jr <keith@caseysoftware.com>
2013-02-22 12:08:27 -0600

    basic settings & config

---
+++ creds-dist.php
@@ -0,0 +1,9 @@
+<?php
+
+$AccountSid = 'ACxxxx';
+$AuthToken  = 'xxxxxx';
+
+$toPhone    = '703-555-1212';
+$toEmail    = 'twilio@example.com';
+$baseURL    = 'http://example.com/subdirectory/';
+$name       = 'Someone';
